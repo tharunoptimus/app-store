0570bfbda99d1323e711a6ba73ff2879108398ec
tharunoptimus <tharunoptimus@outlook.com>
2023-03-22 19:11:38 +0530

    Layout changes and shows app category

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,30 @@
-import Head from 'next/head'
-import Layout from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
-import { siteTitle} from '../components/config'
-import { AuthorSection } from '../components/author'
+import Head from "next/head"
+import Layout from "../components/layout"
+import utilStyles from "../styles/utils.module.css"
+import { getSortedPostsData } from "../lib/posts"
+import { siteTitle } from "../components/config"
+import AppCategory from "../components/AppCard"
 
 export default function Home({ allPostsData }) {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                {title}
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  )
+	return (
+		<Layout home>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<section
+				className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
+			>
+			<AppCategory apps={allPostsData} />
+			</section>
+		</Layout>
+	)
 }
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
+	const allPostsData = getSortedPostsData()
+	return {
+		props: {
+			allPostsData,
+		},
+	}
 }
