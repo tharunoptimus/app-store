284942b0e73a0f785a3e99cf1bf323e1ad1851e7
tharunoptimus <tharunoptimus@outlook.com>
2023-03-24 01:15:40 +0530

    Changes to config

--- components/config.js
+++ components/config.js
@@ -1,5 +1,5 @@
 export let authorName = "Tharunraj Optimus"
-export let siteTitle = `That Blog by ${authorName}`
+export let siteTitle = `Apps by ${authorName}`
 export let aboutAuthor = "Full Stack Web Developer"
 export let blogsAbout = "Blogs about various topics related to computers and stuff"
 export let githubUserName = "tharunoptimus"
