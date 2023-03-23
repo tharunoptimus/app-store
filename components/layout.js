894ae9395abab929bd02abdc7a748f2de76dc24f
tharunoptimus <tharunoptimus@outlook.com>
2023-03-24 01:16:28 +0530

    Favicon is github username pic

--- components/layout.js
+++ components/layout.js
@@ -9,7 +9,7 @@ export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={`https://github.com/${githubUserName}.png`} />
         <meta
           name="description"
           content={siteTitle}
