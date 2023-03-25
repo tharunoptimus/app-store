6b7012281687ba978d6ef5f88a0f29e1050c5822
tharunoptimus <tharunoptimus@outlook.com>
2023-03-26 00:26:19 +0530

    Added typewriting effect to author section

--- components/author.js
+++ components/author.js
@@ -2,26 +2,27 @@ import styles from "./layout.module.css"
 import authorStyles from "./author.module.css"
 import Link from "next/link"
 import { authorName, githubUserName } from "./config"
+import { Typewriter } from "react-simple-typewriter"
 
 export default function Author() {
 	return (
 		<>
 			<Link href="/">
-					<div className={styles.authorContainer}>
-						<div className="authorImageContainer">
-							<img
-								src="/images/profile.png"
-								className={styles.borderCircle}
-								height={40}
-								width={40}
-								alt={authorName}
-							/>
-						</div>
-
-						<div className="authorNameContainer">
-							<span>{authorName}</span>
-						</div>
+				<div className={styles.authorContainer}>
+					<div className="authorImageContainer">
+						<img
+							src="/images/profile.png"
+							className={styles.borderCircle}
+							height={40}
+							width={40}
+							alt={authorName}
+						/>
 					</div>
+
+					<div className="authorNameContainer">
+						<span>{authorName}</span>
+					</div>
+				</div>
 			</Link>
 		</>
 	)
@@ -30,7 +31,14 @@ export default function Author() {
 export function AuthorSection() {
 	return (
 		<p className={authorStyles.aboutAuthor}>
-			{"Apps by "}
+			<Typewriter
+				words={["Web Apps", "PWAs", "Packages", "APIs", "Extensions", "Languages", "Games", "XR Apps", "Apps"]}
+				loop={5}
+				typeSpeed={70}
+				deleteSpeed={50}
+				delaySpeed={1000}
+			/>
+			{" by "}
 			<a
 				href={`https://github.com/${githubUserName}`}
 				target="_blank"
