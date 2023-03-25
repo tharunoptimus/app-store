da0872c922128315ca0c602923003284ebc4a0f4
tharunoptimus <tharunoptimus@outlook.com>
2023-03-26 02:17:56 +0530

    New pic instead of one from github

--- components/layout.js
+++ components/layout.js
@@ -1,48 +1,56 @@
-import Head from 'next/head'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import Head from "next/head"
+import styles from "./layout.module.css"
+import utilStyles from "../styles/utils.module.css"
+import Link from "next/link"
 import { authorName, githubUserName, siteTitle } from "./config"
 import { AuthorSection } from "./author"
 
 export default function Layout({ children, home }) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href={`https://github.com/${githubUserName}.png`} />
-        <meta
-          name="description"
-          content={siteTitle}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <img
-              src={`https://github.com/${githubUserName}.png`}
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={authorName}
-            />
-            <h1 className={utilStyles.heading2Xl}>
-              <AuthorSection />
-            </h1>
-          </>
-        ) : (
-          <></>
-        )}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            ← Back to home
-          </Link>
-        </div>
-      )}
-    </div>
-  )
+	return (
+		<div className={styles.container}>
+			<Head>
+				<link
+					rel="icon"
+					href={`https://github.com/${githubUserName}.png`}
+				/>
+				<meta name="description" content={siteTitle} />
+				<meta name="og:title" content={siteTitle} />
+				<meta name="twitter:card" content="summary_large_image" />
+			</Head>
+			<header className={styles.header}>
+				{home ? (
+					<>
+						<div className={styles.imageHolder}>
+							<img
+								src="/images/laptopguy.webp"
+								className={utilStyles.borderCircle}
+								height={224}
+								width={350}
+								alt={authorName}
+							/>
+							<img
+								src="/images/pic.webp"
+								height={72}
+								width={132}
+								alt={authorName}
+								className={styles.inprintImage}
+							/>
+						</div>
+
+						<h1 className={utilStyles.heading2Xl}>
+							<AuthorSection />
+						</h1>
+					</>
+				) : (
+					<></>
+				)}
+			</header>
+			<main>{children}</main>
+			{!home && (
+				<div className={styles.backToHome}>
+					<Link href="/">← Back to home</Link>
+				</div>
+			)}
+		</div>
+	)
 }
