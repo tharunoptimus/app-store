42ac76971012dd7b97c82b17b920398bca8fe075
tharunoptimus <tharunoptimus@outlook.com>
2023-03-26 00:54:35 +0530

    Pulse input field onclick

--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -1,12 +1,23 @@
 import styles from "./searchBar.module.css"
+import { useRef } from "react"
 
-export default function Search({queryText, handleInputChange}) {
+export default function Search({ queryText, handleInputChange }) {
+	let divRef = useRef(null)
+
+	let handleClick = () => {
+		divRef.current.classList.remove("animate__delay__500ms")
+		divRef.current.classList.add("animate__pulse")
+	}
+
 	return (
-		<div className={`${styles.searchBar} animate__animated animate__delay__500ms animate__jackInTheBox`}>
+		<div
+			className={`${styles.searchBar} animate__animated animate__delay__500ms animate__jackInTheBox`}
+			ref={divRef}
+			onClick={handleClick}
+		>
 			<input
 				type="text"
 				placeholder="Search for apps..."
-				autoFocus={true}
 				value={queryText}
 				onChange={handleInputChange}
 			/>
