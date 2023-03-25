560f118da4b7d3ce72e70796ff6aee5eb7988c8c
tharunoptimus <tharunoptimus@outlook.com>
2023-03-26 00:29:43 +0530

    Added lightspeedleft animation to author div

--- components/author.js
+++ components/author.js
@@ -30,7 +30,7 @@ export default function Author() {
 
 export function AuthorSection() {
 	return (
-		<p className={authorStyles.aboutAuthor}>
+		<p className={`${authorStyles.aboutAuthor} animate__animated animate__lightSpeedInLeft`}>
 			<Typewriter
 				words={["Web Apps", "PWAs", "Packages", "APIs", "Extensions", "Languages", "Games", "XR Apps", "Apps"]}
 				loop={5}
