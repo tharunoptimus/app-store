36acf1a0025a5f97a8fd2eb0f97ec28a6e584573
tharunoptimus <tharunoptimus@outlook.com>
2023-03-26 22:58:53 +0530

    Author component no longer required on posts

--- components/author.js
+++ components/author.js
@@ -7,30 +7,13 @@ import { Typewriter } from "react-simple-typewriter"
 export default function Author() {
 	return (
 		<>
-			<Link href="/">
-				<div className={styles.authorContainer}>
-					<div className="authorImageContainer">
-						<img
-							src="/images/profile.png"
-							className={styles.borderCircle}
-							height={40}
-							width={40}
-							alt={authorName}
-						/>
-					</div>
-
-					<div className="authorNameContainer">
-						<span>{authorName}</span>
-					</div>
-				</div>
-			</Link>
 		</>
 	)
 }
 
 export function AuthorSection() {
 	return (
-		<p className={`${authorStyles.aboutAuthor} animate__animated animate__lightSpeedInLeft`}>
+		<p className={`${authorStyles.aboutAuthor} animate__animated animate__flipInX`}>
 			<Typewriter
 				words={["Web Apps", "PWAs", "Packages", "APIs", "Extensions", "Languages", "Games", "XR Apps", "Apps"]}
 				loop={5}
