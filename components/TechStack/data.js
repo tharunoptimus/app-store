4fc1731eb093c4c55a7ef570f247332b23ff60bc
tharunoptimus <tharunoptimus@outlook.com>
2023-04-01 04:06:39 +0530

    Create Data for Tech Stack

---
+++ components/TechStack/data.js
@@ -0,0 +1,84 @@
+let data = [
+	{
+		title: "A-Frame",
+		imgSrc: "/stack/aframe.svg",
+	},
+	{
+		title: "Android",
+		imgSrc: "/stack/android.svg",
+	},
+	{
+		title: "Bootstrap",
+		imgSrc: "/stack/bootstrap.svg",
+	},
+	{
+		title: "Chrome",
+		imgSrc: "/stack/chrome.svg",
+	},
+	{
+		title: "CSS",
+		imgSrc: "/stack/css.svg",
+	},
+	{
+		title: "Express",
+		imgSrc: "/stack/express.svg",
+	},
+	{
+		title: "Go",
+		imgSrc: "/stack/go.svg",
+	},
+	{
+		title: "HTML",
+		imgSrc: "/stack/html.svg",
+	},
+	{
+		title: "Javascript",
+		imgSrc: "/stack/javascript.svg",
+	},
+	{
+		title: "jQuery",
+		imgSrc: "/stack/jquery.svg",
+	},
+	{
+		title: "MongoDB",
+		imgSrc: "/stack/mongodb.svg",
+	},
+	{
+		title: "Mux",
+		imgSrc: "/stack/mux.svg",
+	},
+	{
+		title: "Next.js",
+		imgSrc: "/stack/nextjs.svg",
+	},
+	{
+		title: "Node.js",
+		imgSrc: "/stack/nodejs.svg",
+	},
+	{
+		title: "NPM",
+		imgSrc: "/stack/npm.svg",
+	},
+	{
+		title: "Pug",
+		imgSrc: "/stack/pug.svg",
+	},
+	{
+		title: "React",
+		imgSrc: "/stack/react.svg",
+	},
+	{
+		title: "Typescript",
+		imgSrc: "/stack/typescript.svg",
+	},
+	{
+		title: "Web Sockets",
+		imgSrc: "/stack/websockets.svg",
+	},
+	{
+		title: "WebXR",
+		imgSrc: "/stack/webxr.svg",
+	},
+]
+
+export default data
