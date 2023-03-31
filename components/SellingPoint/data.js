edbeebb90d2cb9b6271079928329e4bbaea22552
tharunoptimus <tharunoptimus@outlook.com>
2023-04-01 04:07:05 +0530

    Added data for selling point

---
+++ components/SellingPoint/data.js
@@ -0,0 +1,34 @@
+let data = [
+	{
+		title: "Strong problem-solving skills",
+		imgSrc: "/images/skills.webp",
+		description:
+			"I build scalable, efficient, and customized real-world applications using strong problem-solving skills at the core of my development approach",
+	},
+	{
+		title: "Quality-focused development",
+		imgSrc: "/images/quality.webp",
+		description:
+			"Quality is at the heart of everything I do, and I prioritize development practices that ensure efficient, stable, and secure applications that exceed user expectations.",
+	},
+	{
+		title: "Collaborative team player",
+		imgSrc: "/images/collaborator.webp",
+		description:
+			"I value collaboration and teamwork to create exceptional user experiences. My full-stack expertise allows me to work seamlessly with stakeholders to achieve shared goals.",
+	},
+	{
+		title: "Lightning-fast web apps",
+		imgSrc: "/images/lightning.webp",
+		description:
+			"I specialize in creating web apps that load in <2 Seconds, providing exceptional user experiences that drive engagement and success.",
+	},
+	{
+		title: "Offline-first development",
+		imgSrc: "/images/offline.webp",
+		description:
+			"My development approach prioritizes offline functionality, ensuring that users can access and use web apps even when an internet connection is unavailable.",
+	},
+]
+
+export default data
